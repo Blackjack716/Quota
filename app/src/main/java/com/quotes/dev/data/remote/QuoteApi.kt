4051a47d35402d804e4a375250c2ac14896f343a
jBodzak <kubabodzak@o2.pl>
2024-11-15 16:05:23 +0100

    some mistakes was made
    dont judge pls 💔

--- app/src/main/java/com/quotes/dev/data/remote/QuoteApi.kt
+++ app/src/main/java/com/quotes/dev/data/remote/QuoteApi.kt
@@ -1,9 +1,9 @@
 package com.quotes.dev.data.remote
 
 import com.haroldadmin.cnradapter.NetworkResponse
+import com.quotes.dev.BuildConfig
 import com.quotes.dev.data.remote.model.ErrorResponse
 import com.quotes.dev.data.remote.model.QuoteDto
-import com.quotes.dev.data.remote.model.QuoteListDto
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Query
@@ -12,12 +12,12 @@ interface QuoteApi {
 
     @GET("quotes")
     suspend fun getRandomQuote(
-        @Header("X-Api-Key") header: String = "CB4hN38VA/QtaybvBZxOzQ==NDUwNpmygV4ozcNX"
+        @Header("X-Api-Key") header: String = BuildConfig.API_KEY
     ): NetworkResponse<List<QuoteDto>, ErrorResponse>
 
     @GET("quotes")
     suspend fun getRandomQuoteByCategory(
-        @Header("X-Api-Key") header: String = "CB4hN38VA/QtaybvBZxOzQ==NDUwNpmygV4ozcNX",
+        @Header("X-Api-Key") header: String = BuildConfig.API_KEY,
         @Query("category") category: String
     ): NetworkResponse<List<QuoteDto>, ErrorResponse>
 }
